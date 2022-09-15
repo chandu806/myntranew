d5915d185ede4c68128c5b31c1e7277df8648df1
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-09-15 20:11:03 +0530

    myntra

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
-import JoinForm from "./JoinForm";
-import Header from "./Header";
-import "./style.css";
-import Conference from "./Conference";
-import {
-  selectIsConnectedToRoom,
-  useHMSActions,
-  useHMSStore,
-} from "@100mslive/react-sdk";
-import Footer from "./Footer";
+// import React, { useEffect } from "react";
+// import JoinForm from "./JoinForm";
+// import Header from "./Header";
+// import "./style.css";
+// import Conference from "./Conference";
+// import {
+//   selectIsConnectedToRoom,
+//   useHMSActions,
+//   useHMSStore,
+// } from "@100mslive/react-sdk";
+// import Footer from "./Footer";
 
-export const Home = () => {
-  const isConnected = useHMSStore(selectIsConnectedToRoom);
-  const hmsActions = useHMSActions();
+// export const Home = () => {
+//   const isConnected = useHMSStore(selectIsConnectedToRoom);
+//   const hmsActions = useHMSActions();
 
-  useEffect(() => {
-    window.onunload = () => {
-      if (isConnected) {
-        hmsActions.leave();
-      }
-    };
-  }, [hmsActions, isConnected]);
+//   useEffect(() => {
+//     window.onunload = () => {
+//       if (isConnected) {
+//         hmsActions.leave();
+//       }
+//     };
+//   }, [hmsActions, isConnected]);
 
-  return (
-    <div className="App">
-      <Header />
-      {isConnected ? (
-        <>
-          <Conference />
-          <Footer />
-        </>
-      ) : (
-        <JoinForm />
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className="App">
+//       <Header />
+//       {isConnected ? (
+//         <>
+//           <Conference />
+//           <Footer />
+//         </>
+//       ) : (
+//         <JoinForm />
+//       )}
+//     </div>
+//   );
+// };
