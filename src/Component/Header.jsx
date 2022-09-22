284e91ec69618ec5cdc0eec2501e23d9d8d24778
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-09-22 18:02:17 +0530

    myn

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,20 +1,20 @@
-import { selectIsConnectedToRoom, useHMSStore } from "@100mslive/react-sdk";
-import React from "react";
-import Logo from "../Image/logo.png";
+// import { selectIsConnectedToRoom, useHMSStore } from "@100mslive/react-sdk";
+// import React from "react";
+// import Logo from "../Image/logo.png";
 
-function Header() {
-  const isConnected = useHMSStore(selectIsConnectedToRoom);
+// function Header() {
+//   const isConnected = useHMSStore(selectIsConnectedToRoom);
 
-  return (
-    <>
-      {!isConnected && (
-        <header>
-          <img className="logo" src={Logo} alt="logo" />
-          <h2>Welcome To YR Video Conference</h2>
-        </header>
-      )}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {!isConnected && (
+//         <header>
+//           <img className="logo" src={Logo} alt="logo" />
+//           <h2>Welcome To YR Video Conference</h2>
+//         </header>
+//       )}
+//     </>
+//   );
+// }
 
-export default Header;
+// export default Header;
